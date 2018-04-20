ef2448832bfbfed3acdb96cd9b922fb7be570340
包翎 <1004163785@qq.com>
2018-04-20 17:21:15 -0400

    fix the permission problem

--- Get your status/ViewController.swift
+++ Get your status/ViewController.swift
@@ -1,25 +1,63 @@
-//
-//  ViewController.swift
-//  Get your status
-//
-//  Created by 包翎 on 2018/4/20.
-//  Copyright © 2018年 Centennial College. All rights reserved.
-//
-
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    //display realtime info
+    @IBOutlet weak var textView: UITextView!
+    
+    //motion manager object
+    let motionActivityManager = CMMotionActivityManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //get your status
+        startActivityUpdates()
     }
-
+    
+    // get the data
+    func startActivityUpdates() {
+        //identify status
+        guard CMMotionActivityManager.isActivityAvailable() else {
+            self.textView.text = "\n当前设备不支持获取当前运动状态\n"
+            return
+        }
+        let queue = OperationQueue.current
+        self.motionActivityManager.startActivityUpdates(to: queue!, withHandler: {
+            activity in
+            //get all data
+            var text = "---active data---\n"
+            text += "Status: \(activity!.getDescription())\n"
+            if (activity!.confidence == .low) {
+                text += "accuracy: low\n"
+            } else if (activity!.confidence == .medium) {
+                text += "accuracy: medium\n"
+            } else if (activity!.confidence == .high) {
+                text += "accuracy: high\n"
+            }
+            self.textView.text = text
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension CMMotionActivity {
+    /// get description
+    func getDescription() -> String {
+        if self.stationary {
+            return "Still"
+        } else if self.walking {
+            return "Walking"
+        } else if self.running {
+            return "Running"
+        } else if self.automotive {
+            return "Driving"
+        }else if self.cycling {
+            return "Cycling"
+        }
+        return "Unknown"
+    }
+}
